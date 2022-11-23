5aef91c5752999a052981acc63395477dd2330cc
Vann Calhoune <vanncalhoune@Vanns-Air.lan>
2022-11-23 07:23:40 -0600

    Removed search function

--- src/modules/home/TransactionComponent.js
+++ src/modules/home/TransactionComponent.js
@@ -45,26 +45,12 @@ const TransactionCell = (props) => {
 };
 
 const TransactionComponent = (props) => {
-    const [filteredTransaction, updateTxn] = useState(props.transactions);
-    const [searchText, updateSearchText] = useState('');
-    const filterData = () => {
-        if (!searchText || !searchText.trim().length) {
-            updateTxn(props.transactions);
-            return;
-        }
-        let txn = [...props.transactions];
-        txn = txn.filter((payload) => payload.desc.toLowerCase().includes(searchText.toLowerCase().trim())); 
-        updateTxn(txn)
-    };
-
-    useEffect(() => filterData(searchText), [props.transactions]);
+    
 
     return(
         <Container>
-            Transactions
-            <input placeholder='Search' value={searchText} 
-            onChange= {(e) => {updateSearchText(e.target.value); filterData(e.target.value)}} />
-            {filteredTransaction?.length?filteredTransaction.map((payload) => <TransactionCell payload={payload} />) : ""}
+            Transactionsss
+            {props.transactions?.length ? props.transactions.map((payload) => <TransactionCell payload={payload} />) : ""}
             </Container>
     )
 }
